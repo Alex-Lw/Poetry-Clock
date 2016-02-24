9c3aa59c797dd61bebc3785a8e0f15c87a13f066
luodazhi <742335249@qq.com>
2016-02-24 21:07:27 +0800

    change poetry to open a website

--- clock.py
+++ clock.py
@@ -10,7 +10,8 @@ def clocks(duration):
     #verse = open("verse.txt").readlines()          #this is old version for assume that the verse file is regular
     verse=clearVerse("verse.txt")   #this is the new version which will clear the verse file every time
     words = verse[int(len(verse)*random.random())]
-    command = "sleep "+ str(duration)+ " && say '"+words+"'"
+    command = "sleep "+ str(duration)+" && open /Applications/Safari.app http://www.getrelaxed.com/"
+    #command = "sleep "+ str(duration)+ " && say '"+words+"'"
     os.system(command)
 
 def clearVerse(verseaddress):
@@ -36,5 +37,5 @@ if __name__ == '__main__':
         except:
             print "error, please enter it again"
     else:
-        clocks(2400)
+        clocks(3600)
 
